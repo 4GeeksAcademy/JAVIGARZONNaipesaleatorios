8f89b084e830eb2f49e31c4ee8588e074dcb30a0
javigarzon1 <javi.garzon@hotmail.com>
2025-02-05 10:52:49 +0000

    rtr

--- src/app.js
+++ src/app.js
@@ -4,31 +4,27 @@ import "./style.css";
 
 
 
-let ObjetoCarta = [
-let numeroNaipe : ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-let palo :  ['spade', 'club', 'heart','diamond']
-]
+const numeroNaipe = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+const palo =  ['spade', 'club', 'heart','diamond']
 
 function tirarCarta() {
   //*Elegir el palo de la baraja
-const cartaTirada = math.floor(math.random()*ObjetoCarta[numeroNaipe].length);
-const resultado =ObjetoCarta[palo]
+const cartaTirada = math.floor(math.random()*numeroNaipe.length);
 console.log(resultado)
 //* Elegir número de carta
-const paloCarta = math.floor(math.random()*resultado.max + 1);
+const paloCarta = math.floor(math.random()*palo.max + 1);
 
-return (resultado)
-
+return (cartaTirada)
 console.log (paloCarta)
 
 }
-document.getElementById('numero').innerHTML = ObjetoCarta[numeroNaipe[cartaTirada]];
+document.getElementById('numero').innerHTML = numeroNaipe[cartaTirada];
 document.getElementById('naipe').className = "";
 document.getElementById('naipe').classList.add("naipe");
 document.getElementById('naipe').classList.add(getSuiteClass(paloCarta));
 tirarCarta ()
 
-console.log (paloCarta)
+
 
 
 
