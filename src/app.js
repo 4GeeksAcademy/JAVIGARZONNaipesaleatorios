f0ce191f612dbf2a06513e8549364d299cd01928
javigarzon1 <javi.garzon@hotmail.com>
2025-02-05 10:43:06 +0000

    23

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-const ObjetoCarta = [
-let numeroNaipe = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  let palo =  ['spade', 'club', 'heart','diamond'];
+
+let ObjetoCarta = [
+let numeroNaipe : ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+let palo :  ['spade', 'club', 'heart','diamond']
 ]
+
 function tirarCarta() {
   //*Elegir el palo de la baraja
 const cartaTirada = math.floor(math.random()*ObjetoCarta[numeroNaipe].length);
@@ -19,12 +19,16 @@ const paloCarta = math.floor(math.random()*resultado.max + 1);
 
 return (resultado)
 
-return (paloCarta)
+console.log (paloCarta)
 
 }
-
+document.getElementById('numero').innerHTML = ObjetoCarta[numeroNaipe[cartaTirada]];
+document.getElementById('naipe').className = "";
+document.getElementById('naipe').classList.add("naipe");
+document.getElementById('naipe').classList.add(getSuiteClass(paloCarta));
 tirarCarta ()
 
+console.log (paloCarta)
 
 
 
